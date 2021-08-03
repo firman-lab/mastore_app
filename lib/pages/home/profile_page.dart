b51e7c03fe9bf9e8a87c1bbae63535870d8f136a
firman-lab <fhidayat400@gmail.com>
2021-08-04 06:39:24 +0700

    profile page

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -4,11 +4,110 @@ import 'package:mastore_app/theme.dart';
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Profile',
-        style: primaryTextStyle,
-      ),
+    Widget header() {
+      return Container(
+        child: AppBar(
+          backgroundColor: backgroundColor3,
+          automaticallyImplyLeading: false,
+          flexibleSpace: SafeArea(
+            child: Container(
+              padding: EdgeInsets.all(
+                defaultMargin,
+              ),
+              child: Row(
+                children: [
+                  ClipOval(
+                    child: Image.asset(
+                      'assets/pp.png',
+                      width: 64,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Firman Hids',
+                          style: primaryTextStyle.copyWith(
+                            fontSize: 24,
+                            fontWeight: semiBold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          '@upil',
+                          style: subtitleTextStyle.copyWith(
+                            fontSize: 16,
+                            fontWeight: medium,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/pp.png',
+                    width: 28,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget menuItem(String text) {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 16,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [],
+        ),
+      );
+    }
+
+    Widget content() {
+      return Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: backgroundColor3,
+        ),
+        margin: EdgeInsets.all(
+          defaultMargin,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Account',
+              style: primaryTextStyle.copyWith(
+                fontSize: 20,
+                fontWeight: medium,
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Expanded(
+              child: Row(
+                children: [Text('data')],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+        content(),
+      ],
     );
   }
 }
